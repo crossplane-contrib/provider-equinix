fabd9fd55560a1f27118ee8800aa5bad9e817122
hasheddan <georgedanielmangum@gmail.com>
2021-04-23 09:22:21 -0500

    Add ProviderConfig API types
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

---
+++ apis/v1alpha3/provider_types.go
@@ -0,0 +1,52 @@
+/*
+Copyright 2019 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha3
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+)
+
+// A ProviderSpec defines the desired state of a Provider.
+type ProviderSpec struct {
+	// CredentialsSecretRef references a specific secret's key that contains
+	// the credentials that are used to connect to the Equinix API.
+	CredentialsSecretRef xpv1.SecretKeySelector `json:"credentialsSecretRef"`
+}
+
+// +kubebuilder:object:root=true
+
+// A Provider configures an Equinix 'provider'.
+// +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:printcolumn:name="SECRET-NAME",type="string",JSONPath=".spec.credentialsSecretRef.name",priority=1
+// +kubebuilder:resource:scope=Cluster,categories={crossplane,provider,equinix}
+type Provider struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec ProviderSpec `json:"spec"`
+}
+
+// +kubebuilder:object:root=true
+
+// ProviderList contains a list of Provider
+type ProviderList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Provider `json:"items"`
+}
